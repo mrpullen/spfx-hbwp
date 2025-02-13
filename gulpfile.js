23208c95e46336def988beee2686ff3f5a52df41
Michael Rees Pullen <mipullen@microsoft.com>
2025-02-13 10:32:51 -0500

    Add suppressions for specific warnings in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,29 @@ const build = require('@microsoft/sp-build-web');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
+
+build.addSuppression(
+  /.\/node_modules/
+);
+
+
+
+build.addSuppression(
+  `./node_modules/.pnpm/handlebars-helpers*`
+);
+
+build.addSuppression(
+  `./node_modules/.pnpm/ansi-colors@0.2.0*`
+);
+
+build.addSuppression(
+  `./node_modules/.pnpm/create-frame@1.0.0/node_modules/create-frame/utils.js 3:34-41`
+);
+
+build.addSuppression(
+  `Critical dependency: *`
+);
+
 var getTasks = build.rig.getTasks;
 build.rig.getTasks = function () {
   var result = getTasks.call(build.rig);
